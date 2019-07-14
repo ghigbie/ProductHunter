ccb6a89dbf0e04f36df44b83c0226d53569afac3
ghigbie <gehigbie@gmail.com>
2019-07-14 17:44:54 -0600

    Fix file organization

--- product_hunter/products/views.py
+++ product_hunter/products/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, get_object_or_404
 title = 'Product Hunter'
 
 def home(request):
-    return render(request, 'accounts/home.html')
+    return render(request, 'products/home.html')
--- product_hunter/urls.py
+++ product_hunter/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import views
+import products.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', products.views.home, name='home')
 ]
