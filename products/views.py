674016e2d0503e8822b1c73ce9fedd2f0b986951
ghigbie <gehigbie@gmail.com>
2019-07-14 18:32:19 -0600

    Add vars

--- products/views.py
+++ products/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render, get_object_or_404
 title = 'Product Hunter'
 
 def home(request):
-    return render(request, 'products/home.html')
+    return render(request, 'products/home.html',
+                 {'title' : title,
+                 'products': 'products'})
