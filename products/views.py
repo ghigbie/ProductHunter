1a2a851e267c6b99e77275ad7ff20f79292d836f
ghigbie <gehigbie@gmail.com>
2019-07-19 20:54:06 -0600

    Add new products view

--- products/views.py
+++ products/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'products/home.html',
                  {'title' : title,
                  'products': 'products'})
+
+def products(request):
+    return render(request, 'products/products',
+                 {'title' : title,
+                  'products': [1, 2, 3, 4, 5]})
+
