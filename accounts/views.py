89e2b0269d2c61ac7d53ff76f8607c6b7503a175
ghigbie <gehigbie@gmail.com>
2019-07-20 13:36:27 -0600

    Add route details

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
 def signup(request):
     if request.method == 'POST':
         if request.POST['password'] == request.POST['password_confirm']:
-            user = User.objects.get(username=request.POST['username'])
-            return render(request, 'accounts/signup.html',
-                          {'error': 'Username is already in use. Please choose another.'})
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html',
+                            {'error': 'Username is already in use. Please choose another.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                auth.login(request, user)
+                return redirect('home')
     else:
         return render(request, 'accounts/signup.html')
 
