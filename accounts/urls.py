a72798318380a86901135c05ede41aa6e0512483
ghigbie <gehigbie@gmail.com>
2019-07-19 22:20:33 -0600

    Fix template and route errors

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path 
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
-urlpatterrns = [
-    path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='loguout')
+urlpatterns = [
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout")
 ]
--- product_hunter/urls.py
+++ product_hunter/urls.py
@@ -17,11 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import include
-import products.views
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', products.views.home, name='home'),
+    path('', views.home, name='home'),
     path('accounts/', include('accounts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
