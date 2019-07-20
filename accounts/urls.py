ccb098d9bcff4789403a68c4348d2c835d51bd41
ghigbie <gehigbie@gmail.com>
2019-07-19 21:32:08 -0600

    Edit urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterrns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='loguout')
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='loguout')
 ]
--- product_hunter/urls.py
+++ product_hunter/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
-import products
-import accounts
+import products.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.home, name='home'),
-    path('account/', include('account.urls'))
+    path('accounts/', include('accounts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
