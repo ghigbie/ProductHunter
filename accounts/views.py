8f72ee1d53e6355ecc12c494f2e88a6d1363d485
ghigbie <gehigbie@gmail.com>
2019-07-20 13:20:42 -0600

    Add error display method and page

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password_confirm']:
+            user = User.objects.get(username=request.POST['username'])
+            return render(request, 'accounts/signup.html',
+                          {'error': 'Username is already in use. Please choose another.'})
+    else:
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
