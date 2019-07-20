4c14294af46f189b33a03472dd43d6e0044eb259
ghigbie <gehigbie@gmail.com>
2019-07-19 21:02:17 -0600

    Add new path

--- product_hunter/urls.py
+++ product_hunter/urls.py
@@ -19,9 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
 from products import views
+from accounts import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('account/', include('account.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
