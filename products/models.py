07e06e1debfcbef1564a456b3e1636cac92c34d7
ghigbie <gehigbie@gmail.com>
2019-07-21 15:59:55 -0600

    Add product to admin view

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.CharField(max_length=225)
@@ -9,6 +10,8 @@ class Product(models.Model):
     icon = models.ImageField(upload_to='images/')
     votes_total = models.IntegerField(default=1)
 
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.title
     
