acef1f23a294dadb0d05b24ae9ed689edfbe511b
ghigbie <gehigbie@gmail.com>
2019-07-21 16:10:12 -0600

    Add new url patterns

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from djang.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('view/', views.view, name='view')
+    path('create/', views.create, name='create'),
+    path('edit/', views.edit, name='edit'),
+    path('delete/', views.delete, name='delete'),
+]
