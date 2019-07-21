f13e012763da7896af6d6a353e8cd77d5efd1297
ghigbie <gehigbie@gmail.com>
2019-07-21 16:06:34 -0600

    Add url files

--- product_hunter/urls.py
+++ product_hunter/urls.py
@@ -22,5 +22,6 @@ from products import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
