460ae50571afa6d01cb31156d317ab012de2da8b
ghigbie <gehigbie@gmail.com>
2019-07-21 19:45:39 -0600

    Add error fields

--- products/views.py
+++ products/views.py
@@ -15,4 +15,10 @@ def products(request):
 
 @login_required
 def create(request):
-    return render(request, 'products/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and rrequest.POST['url'] and request.POST['icon'] and request.POST['image']:
+        else:
+            return render(request, 'products/create.html', 
+                          {'error': 'All fields are required.'}) 
+    else:
+        return render(request, 'products/create.html')
